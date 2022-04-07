7ebd7989c55dae3662f24e9821376013107769d2
erdemgok <94786448+erdemgok@users.noreply.github.com>
2022-04-07 23:56:42 +0300

    nft

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x866718C35384087BCE1D13f990E71405be21e96A";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
